bbe82f7122f9813fad4ee030996ad800912ec856
i-aman-jha <amanjha.7096@gmail.com>
2026-01-19 20:12:56 +0530

    created gRPC server

--- index.js
+++ index.js
@@ -26,4 +26,68 @@ Advantages of gRPC:
 Protobuf = Data format
 gRPC = Communication system
 ------------------------------------------------------------------------------
-*/ 
+*/
+
+const grpc = require('@grpc/grpc-js');
+const prtoLoader = require('@grpc/proto-loader');
+
+//loading proto in our server
+const packageDefinition = prtoLoader.loadSync("todo.proto", {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+});
+const todosProto = grpc.loadPackageDefinition(packageDefinition); //loading package definition into grpc object
+
+
+const server = new grpc.Server(); //creating grpc server
+
+const todos = [
+    {
+        id: '1',
+        title: 'First Todo',
+        content: 'This is the first todo item'
+    },
+    {
+        id: '2',
+        title: 'Second Todo',
+        content: 'This is the second todo item'
+    }
+];
+
+server.addService(todosProto.TodoService.service, {
+    //we are adding rpcs 
+    listTodos: (call, callback) => {   //all request parameter is going to come in call-> call.param
+        callback(null, todos); //response is sent using callback
+    },
+    createTodo: (call, callback) => {
+        let incomingTodo = call.request; //getting request data from call.request
+        todos.push(incomingTodo); //pushing new todo to todos array
+        callback(null, incomingTodo); //sending response back
+    },
+    getTodo: (call, callback) => {
+        let todoId = call.request.id;
+        let todo = todos.find(t => t.id == todoId);
+        if (todo) {
+            callback(null, todo);
+        } else {
+            callback({
+                code: grpc.status.NOT_FOUND,
+                details: "Todo Not Found"
+            }, null);
+        }
+    }
+});
+
+//starting server
+server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(),
+    (err, port) => {
+        if (err) {
+            console.error("Failed to start server:", err);
+            return;
+        }
+        console.log(`gRPC server running on port ${port}`);
+    }
+);
